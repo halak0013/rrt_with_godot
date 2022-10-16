b40c4cf324f429eaf22fbcc891da5673d57ea302
Muhammet Halak <halakmuhammet145@gmail.com>
2022-10-16 21:56:08 +0300

    rrt finish

---
+++ kodlar/ana.gd
@@ -0,0 +1,46 @@
+#Bismillahirrahmanirrahim
+extends Spatial
+var ek = load("res://kodlar/ek.gd").new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var cordX = Array()
+var cordZ = Array()
+var parents = Array()
+var Swidth
+var SHeight
+var start
+var end
+var enoOfPath=true
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Swidth = $yer.transform.basis.get_scale().x-3
+	SHeight = $yer.transform.basis.get_scale().z-3
+	start = ek.randPoint(Swidth, SHeight)
+	end = ek.randPoint(Swidth, SHeight)
+	ek.drawStartEnd(start, end)
+	cordX.append(start.x)
+	cordZ.append(start.z)
+	parents.append(0)
+	#for n in range(90):
+	#	ek.addNode(Swidth, SHeight, cordX, cordZ, parents)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#ek.drawLine(ek.randPoint(Swidth, SHeight), ek.randPoint(Swidth, SHeight))
+	#print(ek.randPoint(Swidth, SHeight))
+	#if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		if enoOfPath:
+			enoOfPath=ek.addNode(Swidth, SHeight, cordX, cordZ, parents,end,3)
+	#pass
+	
+	
+
+
+func _on_Timer_timeout():
+	#ek.addNode(Swidth, SHeight, cordX, cordZ, parents,end)
+	pass
---
+++ kodlar/ek.gd
@@ -0,0 +1,77 @@
+#Bismillahirrahmanirrahim
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass  # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func randPoint(Swidth, SHeight):
+	randomize()
+	return Vector3(rand_range(-Swidth, Swidth), 0.15, rand_range(-SHeight, SHeight))
+
+
+func drawLine(v1, v2):
+	#randP, smaller
+	add_child(DarwLine3d.DrawLine(v1, v2, Color(0, 0, 1)))
+
+func exitP(v1, end):
+	print(distance(v1, end))
+	if distance(v1, end) < 2:
+		print("başarılı")
+		return true
+	else:
+		return false
+
+
+func distance(v1: Vector3, v2: Vector3):
+	
+	return sqrt(pow(v1.x - v2.x, 2) + pow(v1.z - v2.z, 2))
+
+
+func drawStartEnd(vs: Vector3, ve: Vector3):
+	add_child(DarwLine3d.DrawCube(vs, 1, Color(0, 1, 1)))
+	add_child(DarwLine3d.DrawCube(ve, 1, Color(1, 0, 0)))
+
+
+
+
+func addNode(Swidth, SHeight, cordx, cordz, parents,end,minD):
+	var randP = randPoint(Swidth, SHeight)
+	var smaller = Vector3(cordx[0], 0.15, cordz[0])
+	var n
+	var a= distance(randP, Vector3(cordx[0], 0.15, cordz[0]))
+	var b
+
+	
+	var xp
+	var zp
+	#print("-----------------------------")
+	for p in range(len(cordx) - 1):
+		b = distance(randP, Vector3(cordx[p], 0.15, cordz[p]))
+		#print("a ",a," b ",b)
+		if a > b:
+			smaller = Vector3(cordx[p], 0.15, cordz[p])
+			a=b
+			#print("seçilen    a ",a," b ",b)
+			n = p
+			#print(n)
+
+	parents.append(n)
+	var dis = distance(smaller, randP)
+	xp = (minD*(randP.x-smaller.x))/dis+smaller.x
+	zp = (minD*(randP.z-smaller.z))/dis+smaller.z
+	cordx.append(xp)
+	cordz.append(zp)
+	add_child(DarwLine3d.DrawCube(randP, 0.2, Color(0, 0.5, 0)))
+	drawLine(smaller,Vector3(xp,0.15,zp))
+	return not exitP(Vector3(xp,0.15,zp),end)
+
